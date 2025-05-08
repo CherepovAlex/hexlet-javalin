d6f055c2ca8f5a0792ac3fb874fb2c05061e0cf0
CherepovAlex <westler123@yandex.ru>
2025-05-08 18:57:25 +0300

    2 commit

---
+++ main/src/main/java/org/example1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.example1;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.start(7070);
+    }
+
+}
