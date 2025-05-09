640b29d408c88267ee40ea52d1e6b90c1a547a06
CherepovAlex <westler123@yandex.ru>
2025-05-09 16:43:06 +0300

    3 commit

---
+++ main/src/main/java/org/example2/Hello.java
@@ -0,0 +1,21 @@
+package org.example2;
+
+import io.javalin.Javalin;
+
+public class Hello {
+    public static void main(String[] args) {
+        // Создаем приложение
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        // Описываем, что загрузится по адресу /
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name");
+            if (name == null || name.isEmpty()){
+                name = "World";
+            }
+            ctx.result("Hello" + name + "!");
+        });
+        app.start(7070); // Стартуем веб-сервер
+    }
+}
